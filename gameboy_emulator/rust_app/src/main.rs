5839eebcef225ea07ea1c34f44fb9175d72a2b84
Piotr Zapała <piotrekapala25@gmail.com>
2024-08-12 19:26:44 +0200

    all arithmetic, loading and bit manipulation instructions done

--- gameboy_emulator/rust_app/src/main.rs
+++ gameboy_emulator/rust_app/src/main.rs
@@ -5,11 +5,25 @@ use cpu::CPU;
 use mmu::MMU;
 
 fn main() {
-    let mmu = MMU::new();
+    let mut mmu = MMU::new();
+    mmu.rom[0] = 0xF8;
+    mmu.rom[1] = 255;
+    //mmu.rom[2] = 0xFF;
+    //mmu.write_byte(0xFFEE, 69);
     let mut cpu = CPU::new(mmu);
-    cpu.a = 20;
-    cpu.b = 30;
-    let opcode = 0x80;
-    cpu.execute(opcode);
-    println!("{}", cpu.a);
+    cpu.sp = 4444;
+
+    loop {
+        let opcode = cpu.mmu.fetch_instruction(cpu.pc);
+        cpu.execute(opcode);
+
+        if cpu.pc == 2 {
+            break;
+        }
+    }
+    
+    println!("{:X}", cpu.h);
+    println!("{:X}", cpu.l);
+    //println!("{}", cpu.mmu.read_byte(0xFFEE));
+    //println!("{:X}", cpu.mmu.read_byte(0xC035));
 }
