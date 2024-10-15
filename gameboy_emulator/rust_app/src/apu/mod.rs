620ee63268d7e24b680253c61aecc470ca4c4f59
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-15 12:56:24 +0200

    updated to private fields

--- gameboy_emulator/rust_app/src/apu/mod.rs
+++ gameboy_emulator/rust_app/src/apu/mod.rs
@@ -1,31 +1,31 @@
 pub struct APU {
-    pub nr10: u8, // 0xFF10 - NR10: Channel 1 Sweep Register
-    pub nr11: u8, // 0xFF11 - NR11: Channel 1 Sound Length/Wave Pattern Duty
-    pub nr12: u8, // 0xFF12 - NR12: Channel 1 Volume Envelope
-    pub nr13: u8, // 0xFF13 - NR13: Channel 1 Frequency Low
-    pub nr14: u8, // 0xFF14 - NR14: Channel 1 Frequency High
+    nr10: u8, // 0xFF10 - NR10: Channel 1 Sweep Register
+    nr11: u8, // 0xFF11 - NR11: Channel 1 Sound Length/Wave Pattern Duty
+    nr12: u8, // 0xFF12 - NR12: Channel 1 Volume Envelope
+    nr13: u8, // 0xFF13 - NR13: Channel 1 Frequency Low
+    nr14: u8, // 0xFF14 - NR14: Channel 1 Frequency High
 
-    pub nr21: u8, // 0xFF16 - NR21: Channel 2 Sound Length/Wave Pattern Duty
-    pub nr22: u8, // 0xFF17 - NR22: Channel 2 Volume Envelope
-    pub nr23: u8, // 0xFF18 - NR23: Channel 2 Frequency Low
-    pub nr24: u8, // 0xFF19 - NR24: Channel 2 Frequency High
+    nr21: u8, // 0xFF16 - NR21: Channel 2 Sound Length/Wave Pattern Duty
+    nr22: u8, // 0xFF17 - NR22: Channel 2 Volume Envelope
+    nr23: u8, // 0xFF18 - NR23: Channel 2 Frequency Low
+    nr24: u8, // 0xFF19 - NR24: Channel 2 Frequency High
 
-    pub nr30: u8, // 0xFF1A - NR30: Channel 3 Sound On/Off
-    pub nr31: u8, // 0xFF1B - NR31: Channel 3 Sound Length
-    pub nr32: u8, // 0xFF1C - NR32: Channel 3 Output Level
-    pub nr33: u8, // 0xFF1D - NR33: Channel 3 Frequency Low
-    pub nr34: u8, // 0xFF1E - NR34: Channel 3 Frequency High
+    nr30: u8, // 0xFF1A - NR30: Channel 3 Sound On/Off
+    nr31: u8, // 0xFF1B - NR31: Channel 3 Sound Length
+    nr32: u8, // 0xFF1C - NR32: Channel 3 Output Level
+    nr33: u8, // 0xFF1D - NR33: Channel 3 Frequency Low
+    nr34: u8, // 0xFF1E - NR34: Channel 3 Frequency High
 
-    pub nr41: u8, // 0xFF20 - NR41: Channel 4 Sound Length
-    pub nr42: u8, // 0xFF21 - NR42: Channel 4 Volume Envelope
-    pub nr43: u8, // 0xFF22 - NR43: Channel 4 Polynomial Counter
-    pub nr44: u8, // 0xFF23 - NR44: Channel 4 Counter/Consecutive; Initial
+    nr41: u8, // 0xFF20 - NR41: Channel 4 Sound Length
+    nr42: u8, // 0xFF21 - NR42: Channel 4 Volume Envelope
+    nr43: u8, // 0xFF22 - NR43: Channel 4 Polynomial Counter
+    nr44: u8, // 0xFF23 - NR44: Channel 4 Counter/Consecutive; Initial
 
-    pub nr50: u8, // 0xFF24 - NR50: Channel Control/Volume Control
-    pub nr51: u8, // 0xFF25 - NR51: Selection of Sound Output Terminal
-    pub nr52: u8, // 0xFF26 - NR52: Sound On/Off
+    nr50: u8, // 0xFF24 - NR50: Channel Control/Volume Control
+    nr51: u8, // 0xFF25 - NR51: Selection of Sound Output Terminal
+    nr52: u8, // 0xFF26 - NR52: Sound On/Off
 
-    pub wave_pattern_ram: [u8; 16],
+    wave_pattern_ram: [u8; 16],
 }
 
 impl APU {
