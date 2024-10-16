f1a245341a1b9a23cb02db2383b309cd8649ac46
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-16 15:34:52 +0200

    timer interrupt logic

--- gameboy_emulator/rust_app/src/timer/mod.rs
+++ gameboy_emulator/rust_app/src/timer/mod.rs
@@ -1,9 +1,16 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+use crate::cpu::CPU;
+
 pub struct TIMER {
-    div: u16,   // DIV (Divider Register)
-    tima: u8,   // TIMA (Timer Counter)
-    tma: u8,    // TMA (Timer Modulo)
-    tac: u8,    // TAC (Timer Control)
-    cycles_counter: u16,
+    div: u16,          // DIV (Divider Register)
+    tima: u8,          // TIMA (Timer Counter)
+    tma: u8,           // TMA (Timer Modulo)
+    tac: u8,           // TAC (Timer Control)
+    div_counter: u16,  // Counter for DIV
+    tima_counter: u16, // Counter for TIMA
+    cpu: Option<Rc<RefCell<CPU>>>,
 }
 
 impl TIMER {
@@ -13,16 +20,14 @@ impl TIMER {
             tima: 0,
             tma: 0,
             tac: 0,
-            cycles_counter: 0,
+            div_counter: 0,
+            tima_counter: 0,
+            cpu: None,
         }
     }
 
-    pub fn add_cycles(&mut self, cycles: u16) {
-        self.cycles_counter += cycles;
-        if self.cycles_counter >= 256 {
-            self.div = self.div.wrapping_add(1);
-            self.cycles_counter -= 256;
-        }
+    pub fn set_cpu(&mut self, cpu: Rc<RefCell<CPU>>) {
+        self.cpu = Some(cpu);
     }
 
     pub fn read_byte(&self, address: u16) -> u8 {
@@ -42,6 +47,40 @@ impl TIMER {
             0xFF06 => self.tma = value,
             0xFF07 => self.tac = value,
             _ => panic!("Attempted to write to an invalid memory address: {:04X}", address),
+        }
+    }
+
+    pub fn tick(&mut self, cpu: &mut CPU) {
+        let cycles = cpu.get_cycles();
+
+        self.div_counter += cycles;
+        if self.div_counter >= 256 {
+            self.div = self.div.wrapping_add(1);
+            self.div_counter -= 256;
+        }
+
+        if self.tac & 0b100 != 0 {
+            let clock_select = match self.tac & 0b11 {
+                0b00 => 1024,  // 4096 Hz (1024 cycles CPU)
+                0b01 => 16,    // 262144 Hz (16 cycles CPU)
+                0b10 => 64,    // 65536 Hz (64 cycles CPU)
+                0b11 => 256,   // 16384 Hz (256 cycles CPU)
+                _ => unreachable!(),
+            };
+
+            self.tima_counter += cycles;
+
+            if self.tima_counter >= clock_select {
+                self.tima_counter -= clock_select;
+                self.tima = self.tima.wrapping_add(1);
+
+                if self.tima == 0 {
+                    self.tima = self.tma;
+                    if let Some(ref cpu) = self.cpu {
+                        cpu.borrow_mut().request_interrupt(0b00000100);
+                    }
+                }
+            }
         }
     }
 }
