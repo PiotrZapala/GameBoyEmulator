ed3b6c83dd69025bbe9e50f45382c43de51c88ce
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-16 21:57:51 +0200

    updated lib file

--- gameboy_emulator/rust_app/src/lib.rs
+++ gameboy_emulator/rust_app/src/lib.rs
@@ -1,3 +1,4 @@
+mod bridge_generated; /* AUTO INJECTED BY flutter_rust_bridge. This line may not be accurate, and you can change it according to your needs. */
 pub mod cpu;
 pub mod ppu;
 pub mod apu;
@@ -6,3 +7,8 @@ pub mod timer;
 pub mod utils;
 pub mod joypad;
 pub mod cartridge;
+pub mod emulator;
+
+mod api;
+
+pub use api::*;
