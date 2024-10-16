c714fc865135ae614fbd0ee39bc76b36da32736c
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-16 21:57:37 +0200

    changed main file to api file

---
+++ gameboy_emulator/rust_app/src/api.rs
@@ -0,0 +1,39 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+use flutter_rust_bridge::frb;
+
+use crate::cartridge::CARTRIDGE;
+use crate::emulator::EMULATOR;
+
+lazy_static! {
+    static ref EMULATOR_INSTANCE: Mutex<Option<EMULATOR>> = Mutex::new(None);
+}
+
+pub fn load_rom(rom_data: Vec<u8>) {
+    let cartridge = CARTRIDGE::new(rom_data);
+    let emulator = EMULATOR::new(cartridge);
+
+    let mut emulator_instance = EMULATOR_INSTANCE.lock().unwrap();
+    *emulator_instance = Some(emulator);
+}
+
+pub fn render_frame() -> Option<Vec<u32>> {
+    let mut emulator_instance = EMULATOR_INSTANCE.lock().unwrap();
+    
+    if let Some(ref mut emulator) = *emulator_instance {
+        Some(emulator.render_frame())
+    } else {
+        None
+    }
+}
+
+#[frb]
+pub fn load_rom_api(rom_data: Vec<u8>) {
+    load_rom(rom_data);
+}
+
+#[frb]
+pub fn render_frame_api() -> Option<Vec<u32>> {
+    render_frame()
+}
