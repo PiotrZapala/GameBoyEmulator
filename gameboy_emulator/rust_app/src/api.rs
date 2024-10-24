f66f31bd462486d7dd3197d78c4190ed85dcab59
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-24 18:01:52 +0200

    new api for ffi

--- gameboy_emulator/rust_app/src/api.rs
+++ gameboy_emulator/rust_app/src/api.rs
@@ -28,6 +28,33 @@ pub fn render_frame() -> Option<Vec<u32>> {
     }
 }
 
+pub fn handle_vblank() {
+    let mut emulator_instance = EMULATOR_INSTANCE.lock().unwrap();
+    
+    if let Some(ref mut emulator) = *emulator_instance {
+        emulator.handle_vblank();
+    }
+}
+
+pub fn set_buttons_state(button_states: Vec<u8>) {
+    let mut emulator_instance = EMULATOR_INSTANCE.lock().unwrap();
+    
+    if let Some(ref mut emulator) = *emulator_instance {
+        if button_states.len() == 8 {
+            emulator.set_button_states(
+                button_states[0], // Up
+                button_states[1], // Down
+                button_states[2], // Left
+                button_states[3], // Right
+                button_states[4], // A
+                button_states[5], // B
+                button_states[6], // Start
+                button_states[7], // Select
+            );
+        }
+    }
+}
+
 #[frb]
 pub fn load(rom_data: Vec<u8>) {
     load_rom(rom_data);
@@ -36,4 +63,14 @@ pub fn load(rom_data: Vec<u8>) {
 #[frb]
 pub fn render() -> Option<Vec<u32>> {
     render_frame()
+}
+
+#[frb]
+pub fn vblank() {
+    handle_vblank();
+}
+
+#[frb]
+pub fn set_buttons(button_states: Vec<u8>) {
+    set_buttons_state(button_states);
 }
