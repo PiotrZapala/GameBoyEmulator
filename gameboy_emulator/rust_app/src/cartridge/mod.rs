0e3e66a9f381d1ea882f2a413912208318bf9e96
Piotr Zapała <piotrekapala25@gmail.com>
2024-10-30 12:38:32 +0100

    fixed mapping of ram size

--- gameboy_emulator/rust_app/src/cartridge/mod.rs
+++ gameboy_emulator/rust_app/src/cartridge/mod.rs
@@ -9,7 +9,7 @@ pub struct CARTRIDGE {
     pub sgb_flag: u8,                // 0x0146
     pub cartridge_type: u8,          // 0x0147
     pub rom_size: u8,                // 0x0148
-    pub ram_size: u8,                // 0x0149
+    pub ram_size: Option<u32>,       // 0x0149
     pub destination_code: u8,        // 0x014A
     pub old_licensee_code: u8,       // 0x014B
     pub mask_rom_version_number: u8, // 0x014C
@@ -19,6 +19,16 @@ pub struct CARTRIDGE {
 
 impl CARTRIDGE {
     pub fn new(data: Vec<u8>) -> Self {
+        let ram_size = match data.get(0x0149) {
+            Some(0x00) => None,             
+            Some(0x01) => Some(2 * 1024),   
+            Some(0x02) => Some(8 * 1024),   
+            Some(0x03) => Some(32 * 1024),  
+            Some(0x04) => Some(128 * 1024), 
+            Some(0x05) => Some(64 * 1024),  
+            _ => None,                      
+        };
+
         Self {
             entry_point: [data[0x0100], data[0x0101], data[0x0102], data[0x0103]],
             nintendo_logo: data[0x0104..0x0134].try_into().unwrap(),
@@ -29,7 +39,7 @@ impl CARTRIDGE {
             sgb_flag: data[0x0146],
             cartridge_type: data[0x0147],
             rom_size: data[0x0148],
-            ram_size: data[0x0149],
+            ram_size,
             destination_code: data[0x014A],
             old_licensee_code: data[0x014B],
             mask_rom_version_number: data[0x014C],
