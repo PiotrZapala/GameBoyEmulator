7f43f7177857106c7c55764feab5dd3576667e34
Piotr Zapała <piotrekapala25@gmail.com>
2024-11-24 14:48:41 +0100

    updated scripts

--- gameboy_emulator/bridge_generator/flutter_rust_bridge_codegen.sh
+++ gameboy_emulator/bridge_generator/flutter_rust_bridge_codegen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 flutter_rust_bridge_codegen \
-  -r rust_app/src/api.rs \
-  -d flutter_app/lib/bridge_generated.dart\
-  --dart-decl-output flutter_app/lib/bridge_definitions.dart
+  -r ../rust_core/src/api.rs \
+  -d ../flutter_interface/lib/bridge_generated.dart\
+  --dart-decl-output ../flutter_interface/lib/bridge_definitions.dart
--- gameboy_emulator/bridge_generator/flutter_rust_bridge_codegen_ios.sh
+++ gameboy_emulator/bridge_generator/flutter_rust_bridge_codegen_ios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 flutter_rust_bridge_codegen \
-  -r rust_app/src/api.rs \
-  -d flutter_app/lib/bridge_generated.dart\
-  --dart-decl-output flutter_app/lib/bridge_definitions.dart \
-  -c flutter_app/ios/Runner/bridge_generated.h
+  -r ../rust_core/src/api.rs \
+  -d ../flutter_interface/lib/bridge_generated.dart\
+  --dart-decl-output ../flutter_interface/lib/bridge_definitions.dart \
+  -c ../flutter_interface/ios/Runner/bridge_generated.h
